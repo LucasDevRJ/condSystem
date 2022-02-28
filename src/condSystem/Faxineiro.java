8cb1fe62504704e62bfb5dbc8f13d1cf1128b4f6
Lucas <lucaspereiradelima2020@gmail.com>
2022-02-28 17:23:26 -0300

    Classe Pedreiro finalizada

--- src/condSystem/Faxineiro.java
+++ src/condSystem/Faxineiro.java
@@ -5,9 +5,9 @@ public class Faxineiro extends Colaborador {
 	private boolean estaLimpo;
 
 	public void faxinar(String local) {
-		if (isEstaLimpo() == false) {
-			System.out.println("Faxineiro esta faxinando o " + local + ".");
-			this.estaLimpo = true;
+		if (this.isEstaLimpo() == false) {
+			System.out.println("Faxineiro " + super.getInformacoesPessoais().getNome() + " " + super.getInformacoesPessoais().getSobrenome() + " esta faxinando o " + local + ".");
+			this.setEstaLimpo(true);
 		} else {
 			System.out.println("O " + local + " já esta limpo.");
 		}
