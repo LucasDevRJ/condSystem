2493866e0e78f891cf4eb563f4299c6ff2360e2b
Lucas <lucaspereiradelima2020@gmail.com>
2022-03-03 20:46:44 -0300

    Adição de métodos na classe Eletricista

--- src/condSystem/Eletricista.java
+++ src/condSystem/Eletricista.java
@@ -2,5 +2,11 @@ package condSystem;
 
 public class Eletricista extends Colaborador {
 
+	public void trocaFiacao() {
+		System.out.println("O eletricista " + super.getInformacoesPessoais().getNome() + " " + super.getInformacoesPessoais().getSobrenome() + " esta trocando a fiação.");
+	}
 	
+	public void consertaFiacao() {
+		System.out.println("O eletricista " + super.getInformacoesPessoais().getNome() + " " + super.getInformacoesPessoais().getSobrenome() + " esta consertando a fiação.");
+	}
 }
