6c2ed84a7a7dc8b14b6e7e15b435da8f647ea920
Lucas <lucaspereiradelima2020@gmail.com>
2022-04-03 20:22:53 -0300

    Adição de comentário

--- src/com/github/lucasdevrj/condsystem/anotacoes/Autor.java
+++ src/com/github/lucasdevrj/condsystem/anotacoes/Autor.java
@@ -1,5 +1,9 @@
 package com.github.lucasdevrj.condsystem.anotacoes;
-
+/**
+ * Anotações do Projeto
+ * @author lucas
+ *
+ */
 public @interface Autor {
 
 	String autor() default "Lucas Pereira de Lima";
