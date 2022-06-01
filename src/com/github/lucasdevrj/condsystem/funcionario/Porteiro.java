e4720c920be8b78412f3ccff08ff7d18f4b9881d
Lucas <lucaspereiradelima2020@gmail.com>
2022-06-01 08:54:09 -0300

    Exclusão da Classe GravarArquivoPorteiro e adição do fluxo de dados na Classe Porteiro

--- src/com/github/lucasdevrj/condsystem/funcionario/Porteiro.java
+++ src/com/github/lucasdevrj/condsystem/funcionario/Porteiro.java
@@ -1,6 +1,8 @@
 package com.github.lucasdevrj.condsystem.funcionario;
 
-import com.github.lucasdevrj.condsystem.gravacoes.GravarArquivoPorteiro;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import com.github.lucasdevrj.condsystem.leituras.LeituraArquivo;
 
 /**
@@ -10,36 +12,64 @@ import com.github.lucasdevrj.condsystem.leituras.LeituraArquivo;
  */
 public class Porteiro extends Colaborador {
 	
-	private static boolean portaEstaAberta;
-	private static boolean portaoEstaAberto;
+	private boolean portaEstaAberta;
+	private boolean portaoEstaAberto;
 	/**
 	 * Método para o Porteiro abrir a porta, que possui uma lógica para verificar se a porta esta aberta ou fechada.
 	 */
-	public void abrirPorta(Porteiro porteiro) {
-		GravarArquivoPorteiro.gravarAberturaPorta(porteiro);
+	public void abrirPorta() {
+		try {
+			PrintWriter grava = new PrintWriter("arquivos.txt");
+			
+			if (this.isPortaEstaAberta() == false) {
+				grava.println("O porteiro " + super.getInformacoesPessoais().getNome() +  " " + super.getInformacoesPessoais().getSobrenome() + " abriu a porta.");
+				this.setPortaEstaAberta(true);
+			} else {
+				grava.println("A porta já esta aberta!");
+			}
+			
+			grava.close();
+			
+		} catch (IOException erro) {
+			erro.printStackTrace();
+		}
 		LeituraArquivo.lerArquivo();
 	}
 	/**
 	 * Método para o Porteiro abrir o portão, que possui uma lógica para verificar se a portão esta aberta ou fechada.
 	 */
-	public void abrirPortao(Porteiro porteiro) {
-		GravarArquivoPorteiro.gravarAberturaPortao(porteiro);
+	public void abrirPortao() {
+		try {
+			PrintWriter grava = new PrintWriter("arquivos.txt");
+			
+			if (this.isPortaoEstaAberto() == false) {
+				grava.println("O porteiro " + super.getInformacoesPessoais().getNome() +  " " + super.getInformacoesPessoais().getSobrenome() + " abriu o portão.");
+				this.setPortaoEstaAberto(true);
+			} else {
+				grava.println("O portão já esta aberto!");
+			}
+			
+			grava.close();
+			
+		} catch (IOException erro) {
+			erro.printStackTrace();
+		}
 		LeituraArquivo.lerArquivo();
 	}
 	
-	public static boolean isPortaEstaAberta() {
+	public boolean isPortaEstaAberta() {
 		return portaEstaAberta;
 	}
 	
-	public static void setPortaEstaAberta(boolean portaEstaAberta) {
-		Porteiro.portaEstaAberta = portaEstaAberta;
+	public void setPortaEstaAberta(boolean portaEstaAberta) {
+		this.portaEstaAberta = portaEstaAberta;
 	}
 	
-	public static boolean isPortaoEstaAberto() {
+	public boolean isPortaoEstaAberto() {
 		return portaoEstaAberto;
 	}
 	
-	public static void setPortaoEstaAberto(boolean portaoEstaAberto) {
-		Porteiro.portaoEstaAberto = portaoEstaAberto;
+	public void setPortaoEstaAberto(boolean portaoEstaAberto) {
+		this.portaoEstaAberto = portaoEstaAberto;
 	}
 }
